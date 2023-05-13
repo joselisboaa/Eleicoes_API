30de79f0c33426e0974418dac7a81ac351f53a4a
1mmanuelKant <joselisboaa05@gmail.com>
2023-05-13 13:55:21 -0300

    chore: sqlalchemy configs

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
+import os
 from flask import Flask
 from flask_smorest import Api
+from db import db
+import models
 
-app = Flask(__name__)
+def create_app():
+      
+  app = Flask(__name__)
 
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "Election REST API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/api"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+  app.config["PROPAGATE_EXCEPTIONS"] = True
+  app.config["API_TITLE"] = "Election REST API"
+  app.config["API_VERSION"] = "v1"
+  app.config["OPENAPI_VERSION"] = "3.0.3"
+  app.config["OPENAPI_URL_PREFIX"] = "/"
+  app.config["OPENAPI_SWAGGER_UI_PATH"] = "/api"
+  app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+  app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 
-api = Api(app)
+  api = Api(app)
+
+  return app
