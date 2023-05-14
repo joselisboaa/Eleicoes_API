5157e61d85116a14cd7eb1df9d5ce6b35c757090
1mmanuelKant <joselisboaa05@gmail.com>
2023-05-13 22:17:37 -0300

    feat(EA001): fix variable name of city foreign key

--- models/electoral_zone.py
+++ models/electoral_zone.py
@@ -4,7 +4,7 @@ class ElectoralZoneModel(db.Model):
     __tablename__ = "electoral_zones"
 
     id = db.Column(db.Integer, primary_key=True)
-    id_city = db.Column(db.Integer, db.ForeignKey("cities.id"), unique=False, nullable=False, required=True)
+    city_id = db.Column(db.Integer, db.ForeignKey("cities.id"), unique=False, nullable=False, required=True)
     name = db.Column(db.String(15), unique=True, nullable=False, required=True)
 
     city = db.relationship("CityModel", backpopulates="electoral_zones")
